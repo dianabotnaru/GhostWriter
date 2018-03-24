0e3ad856cd25a6f5500c4d1a6accc70f67fe5e18
Diana Botnaru <botnaru.diana@yahoo.com>
2018-03-23 20:01:57 -0700

    added splash screen

--- app/src/main/java/com/mudib/ghostwriter/activity/MainActivity.java
+++ app/src/main/java/com/mudib/ghostwriter/activity/MainActivity.java
@@ -1,7 +1,12 @@
 package com.mudib.ghostwriter.activity;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.mudib.ghostwriter.R;
 import com.mudib.ghostwriter.manager.SearchImagesCacheManager;
@@ -14,17 +19,35 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        SearchImagesCacheManager.with().startFetchFlickrImages();
+        setContentView(R.layout.activity_splash);
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }else{
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            getSupportActionBar().hide();
+        }
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, ImageDisplayActivity.class));
+                finish();
+            }
+        }, 3000);
+
+//        SearchImagesCacheManager.with().startFetchFlickrImages();
     }
 
-    @OnClick(R.id.start_btn)
-    public void onStartClick() {
-        startActivity(new Intent(this, ImageDisplayActivity.class));
-    }
-
-    @OnClick(R.id.setting_btn)
-    public void onSettingClick() {
-        startActivity(new Intent(this, SettingActivity.class));
-    }
+//    @OnClick(R.id.start_btn)
+//    public void onStartClick() {
+//        startActivity(new Intent(this, ImageDisplayActivity.class));
+//    }
+//
+//    @OnClick(R.id.setting_btn)
+//    public void onSettingClick() {
+//        startActivity(new Intent(this, SettingActivity.class));
+//    }
 }
