e6112969207875278d23554a3480826083362a9f
Diana Botnaru <botnaru.diana@yahoo.com>
2018-04-30 18:44:23 -0700

    fixed a minor issue

--- app/src/main/java/com/mudib/ghostwriter/utils/DataCovertUtil.java
+++ app/src/main/java/com/mudib/ghostwriter/utils/DataCovertUtil.java
@@ -5,6 +5,7 @@ import com.mudib.ghostwriter.models.Keyword;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by jordi on 30/04/2018.
@@ -20,13 +21,13 @@ public class DataCovertUtil {
     }
 
     public static String getCurrentTimeString(){
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
         String currentDateandTime = sdf.format(new Date());
         return currentDateandTime;
     }
 
     public static String getCurrentTimeStringforID(){
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss",Locale.US);
         String currentDateandTime = sdf.format(new Date());
         return currentDateandTime;
     }
